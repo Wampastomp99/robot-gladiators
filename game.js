8efd08eba0b3fd4605946f59f7643a802e43f55e
Wampastomp99 <michaelheartbound99@hotmail.com>
2021-02-04 18:17:54 -0700

    other changes

--- game.js
+++ game.js
@@ -4,24 +4,12 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-<<<<<<< HEAD
-console.log(playerName);
-console.log(playerHealth, playerAttack, playerMoney);
-console.log("You can put regular sentences in these.");
-
-var enemyName = "Roborto";
-=======
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
->>>>>>> 24cec259a9c65e583c106a3a788b1fca4d00b3c3
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-<<<<<<< HEAD
-var fight = function() {
-=======
 var fight = function(enemyName) {
->>>>>>> 24cec259a9c65e583c106a3a788b1fca4d00b3c3
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -77,9 +65,6 @@ var fight = function(enemyName) {
 };
 
 // run fight function to start game
-<<<<<<< HEAD
-fight();
-=======
 for(var i = 0; i < enemyNames.length; i++) {
   fight(enemyNames[i]);
 }
@@ -87,4 +72,3 @@ for(var i = 0; i < enemyNames.length; i++) {
 
 
 
->>>>>>> 24cec259a9c65e583c106a3a788b1fca4d00b3c3
